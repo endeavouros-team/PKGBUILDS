c2069e7202fa033fb8c807d5b6249a09a79fa4d7
Johannes Kamprad <archlinux@kamprad.net>
2020-01-04 23:01:26 +0100

    Update translations-welcome-de.bash

--- welcome/translations-welcome-de.bash
+++ welcome/translations-welcome-de.bash
@@ -12,11 +12,11 @@
 #    <placeholder>   A pre-defined name that identifies the place in the Welcome app where this string is used.
 #    "string"        The translated string for the Welcome app.
 
-# English:
+# German:
 
 ### First some potentially useful definitions:
 
-_tr_lang=en            # helper variable
+_tr_lang=de            # helper variable
 
 # Help with some special characters (HTML). Yad has problems without them:
 _exclamation='&#33;'   # '!'
@@ -26,93 +26,93 @@ _question='&#63;'      # '?'
 
 ### Now the actual strings to be translated:
 
-_tr_add $_tr_lang welcome_disabled      "$PRETTY_PROGNAME app is disabled. To start it anyway, use option --enable."
+_tr_add $_tr_lang welcome_disabled      "$PRETTY_PROGNAME app ist deaktiviert. Um sie trotzdem zu starten, benutze --enable."
 
-_tr_add $_tr_lang butt_later            "See you later"
-_tr_add $_tr_lang butt_latertip         "Keep $PRETTY_PROGNAME enabled"
+_tr_add $_tr_lang butt_later            "Bis Später"
+_tr_add $_tr_lang butt_latertip         "Behalte $PRETTY_PROGNAME aktiviert"
 
-_tr_add $_tr_lang butt_noshow           "Don't show me anymore"
-_tr_add $_tr_lang butt_noshowtip        "Disable $PRETTY_PROGNAME"
+_tr_add $_tr_lang butt_noshow           "Nicht mehr automatisch starten"
+_tr_add $_tr_lang butt_noshowtip        "Deaktiviere $PRETTY_PROGNAME"
 
-_tr_add $_tr_lang butt_help             "Help"
-
-
-_tr_add $_tr_lang nb_tab_INSTALL        "INSTALL"
-_tr_add $_tr_lang nb_tab_GeneralInfo    "GeneralInfo"
-_tr_add $_tr_lang nb_tab_AfterInstall   "AfterInstall"
-_tr_add $_tr_lang nb_tab_AddMoreApps    "AddMoreApps"
+_tr_add $_tr_lang butt_help             "Hilfe"
 
 
-_tr_add $_tr_lang after_install_text    "After install tasks"
-
-_tr_add $_tr_lang after_install_um      "Update Mirrors"
-_tr_add $_tr_lang after_install_umtip   "Update list of mirrors before system update"
-
-_tr_add $_tr_lang after_install_us      "Update System"
-_tr_add $_tr_lang after_install_ustip   "Update System Software"
-
-_tr_add $_tr_lang after_install_dsi     "Detect system issues"
-_tr_add $_tr_lang after_install_dsitip  "Detect any potential issues on system packages or elsewhere"
-
-_tr_add $_tr_lang after_install_etl     "EndeavourOS to latest$_question"
-_tr_add $_tr_lang after_install_etltip  "Show what to do to get your system to the latest EndeavourOS level"
-
-_tr_add $_tr_lang after_install_cdm     "Change Display Manager"
-_tr_add $_tr_lang after_install_cdmtip  "Use a different display manager"
-
-_tr_add $_tr_lang after_install_ew      "EndeavourOS wallpaper"
-_tr_add $_tr_lang after_install_ewtip   "Change desktop wallpaper to EOS default"
+_tr_add $_tr_lang nb_tab_INSTALL        "Installiere"
+_tr_add $_tr_lang nb_tab_GeneralInfo    "Allgemeine_Informationen"
+_tr_add $_tr_lang nb_tab_AfterInstall   "Nach_der_Installation"
+_tr_add $_tr_lang nb_tab_AddMoreApps    "Füge_mehr_Programme_hinzu"
 
 
-_tr_add $_tr_lang after_install_pm      "Package management"
-_tr_add $_tr_lang after_install_pmtip   "How to manage packages with pacman"
+_tr_add $_tr_lang after_install_text    "Erledigungen nach der Installation"
+
+_tr_add $_tr_lang after_install_um      "Erneuere die Spiegelserver Liste"
+_tr_add $_tr_lang after_install_umtip   "Erneuere die Liste der Spiegelserver vor dem Systemupdate"
+
+_tr_add $_tr_lang after_install_us      "System Update"
+_tr_add $_tr_lang after_install_ustip   "System Software Update"
+
+_tr_add $_tr_lang after_install_dsi     "Suche nach System Problemen"
+_tr_add $_tr_lang after_install_dsitip  "Sucht nach Problemen mit Systemeinstellungen oder Paketen"
+
+_tr_add $_tr_lang after_install_etl     "EndeavourOS auf dem letzten Stand$_question"
+_tr_add $_tr_lang after_install_etltip  "Zeigt an was zu tun ist um das System auf den neuesten EndeavourOS Stand zu bringen"
+
+_tr_add $_tr_lang after_install_cdm     "Ändere den Login-Manager"
+_tr_add $_tr_lang after_install_cdmtip  "benutze einen anderen Login-Manager"
+
+_tr_add $_tr_lang after_install_ew      "EndeavourOS Hintergrund"
+_tr_add $_tr_lang after_install_ewtip   "Benutze den EndeavourOS Bildschirm-Hintergrund"
+
+
+_tr_add $_tr_lang after_install_pm      "Paket Verwaltung"
+_tr_add $_tr_lang after_install_pmtip   "Wie verwalte ich meine Pakete mit pacman"
 
 _tr_add $_tr_lang after_install_ay      "AUR $_and yay$_exclamation"
-_tr_add $_tr_lang after_install_aytip   "Arch User Repository and yay info"
+_tr_add $_tr_lang after_install_aytip   "Arch User Repository und yay info"
 
-_tr_add $_tr_lang after_install_hn      "Hardware and Network"
-_tr_add $_tr_lang after_install_hntip   "Get your hardware working"
+_tr_add $_tr_lang after_install_hn      "Hardware und Netzwerk"
+_tr_add $_tr_lang after_install_hntip   "Bring deine Hardware zum Laufen"
 
 _tr_add $_tr_lang after_install_bt      "Bluetooth"
-_tr_add $_tr_lang after_install_bttip   "Bluetooth advice"
+_tr_add $_tr_lang after_install_bttip   "Bluetooth Informationen"
 
-_tr_add $_tr_lang after_install_nv      "NVIDIA users$_exclamation"
-_tr_add $_tr_lang after_install_nvtip   "Use NVIDIA installer"
+_tr_add $_tr_lang after_install_nv      "NVIDIA Nutzer$_exclamation"
+_tr_add $_tr_lang after_install_nvtip   "Wie funktioniert der Nvidia-Installer"
 
 _tr_add $_tr_lang after_install_ft      "Forum tips"
-_tr_add $_tr_lang after_install_fttip   "Help us help you!"
+_tr_add $_tr_lang after_install_fttip   "Hilf uns dir zu helfen!"
 
 
-_tr_add $_tr_lang general_info_text     "Find your way at the EndeavourOS website$_exclamation"
+_tr_add $_tr_lang general_info_text     "Finde dich auf der EndeavourOS Webseite zurecht$_exclamation"
 
-_tr_add $_tr_lang general_info_ws       "Web site"
+_tr_add $_tr_lang general_info_ws       "Webseite"
 
 _tr_add $_tr_lang general_info_wi       "Wiki"
-_tr_add $_tr_lang general_info_witip    "Featured articles"
+_tr_add $_tr_lang general_info_witip    "Interessante Einträge"
 
-_tr_add $_tr_lang general_info_ne       "News"
-_tr_add $_tr_lang general_info_netip    "News and articles"
+_tr_add $_tr_lang general_info_ne       "Neuigkeiten"
+_tr_add $_tr_lang general_info_netip    "Neuigkeiten und Berichte"
 
 _tr_add $_tr_lang general_info_fo       "Forum"
-_tr_add $_tr_lang general_info_fotip    "Ask, comment, and chat in our friendly forum!"
+_tr_add $_tr_lang general_info_fotip    "Erhalte Hilfe in unserem freundlichen und offenem Forum"
 
-_tr_add $_tr_lang general_info_do       "Donate"
-_tr_add $_tr_lang general_info_dotip    "Help us keep EndeavourOS running"
+_tr_add $_tr_lang general_info_do       "Spenden"
+_tr_add $_tr_lang general_info_dotip    "Unterstütze unsere Arbeit mit einer Spende"
 
-_tr_add $_tr_lang general_info_ab       "About $PRETTY_PROGNAME"
-_tr_add $_tr_lang general_info_abtip    "More info about this app"
+_tr_add $_tr_lang general_info_ab       "Über $PRETTY_PROGNAME"
+_tr_add $_tr_lang general_info_abtip    "Wichtige Informationen über diese App"
 
 
-_tr_add $_tr_lang add_more_apps_text    "Install popular apps"
+_tr_add $_tr_lang add_more_apps_text    "Installiere einige nützliche Programme"
 
-_tr_add $_tr_lang add_more_apps_lotip   "Office tools (libreoffice-fresh)"
+_tr_add $_tr_lang add_more_apps_lotip   "Office Anwendung (libreoffice-fresh)"
 
 _tr_add $_tr_lang add_more_apps_ch      "Chromium Web Browser"
 _tr_add $_tr_lang add_more_apps_chtip   "Web Browser"
 
 _tr_add $_tr_lang add_more_apps_fw      "Firewall"
-_tr_add $_tr_lang add_more_apps_fwtip   "Gufw firewall"
+_tr_add $_tr_lang add_more_apps_fwtip   "Gufw Firewall"
 
-_tr_add $_tr_lang add_more_apps_bt      "Bluetooth Manager for Xfce"
+_tr_add $_tr_lang add_more_apps_bt      "Bluetooth Manager für Xfce"
 _tr_add $_tr_lang add_more_apps_bttip   "Blueberry"
 
