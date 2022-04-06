8e8f971db8046fe2a5e8e50ae283b53d192436c9
manuel-192 <manuel-192@outlook.com>
2022-04-06 20:16:29 +0300

    [eos-translations] more EN and FI translations

--- eos-translations/translation-fi.bash
+++ eos-translations/translation-fi.bash
@@ -127,7 +127,7 @@ _tr_add2 add_more_apps_bt_bm   "Bluetooth (blueman) Xfce:lle"
 
 ####################### NEW STUFF AFTER THIS LINE:
 
-_tr_add2 settings_dis_contents   "Saat $PRETTY_PROGNAME:n taas käyttöön kun ajat komennon:\n<tt>   $PROGNAME --enable</tt>\n"
+_tr_add2 settings_dis_contents   "Saat $PRETTY_PROGNAME:n taas käyttöön kun ajat komennon:\n<tt>   $PROGNAME --enable</tt>\ntai\n<tt>   $PROGNAME --once</tt>\n"  # changed 2022-Apr-06
 _tr_add2 settings_dis_text       "Ota uudestaan käyttöön $PRETTY_PROGNAME:"
 _tr_add2 settings_dis_title      "Kuinka otat uudestaan käyttöön $PRETTY_PROGNAME:n"
 _tr_add2 settings_dis_butt       "Selvä"
