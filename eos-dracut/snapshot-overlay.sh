9b725d9c0caaa33ce78f78ed2aaa67a6743b051c
dalto <dalto@fastmail.com>
2024-04-28 15:09:20 -0500

    [eos-dracut] Bring in kernel 6.8 fixes from Garuda

--- eos-dracut/snapshot-overlay.sh
+++ eos-dracut/snapshot-overlay.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 function mount_snapshot_overlay() {
     local root_mnt="$NEWROOT"
-    if [[ "$(findmnt --mountpoint "$root_mnt" -o FSTYPE -n)" = "btrfs" ]] && [[ "$(btrfs property get ${root_mnt} ro)" != "ro=false" ]]; then
+    local UUID FSTYPE
+    IFS=" " read -r UUID FSTYPE < <(findmnt --mountpoint "$root_mnt" -o UUID,FSTYPE -n)
+    if [[ "$FSTYPE" = "btrfs" ]] && [[ "$(btrfs property get "${root_mnt}" ro)" == "ro=true" ]]; then
         local ram_dir=$(mktemp -d -p /)
+
+        # we are required to mount the top btrfs volume and then remount it as read-write explicitly to avoid all subvolumes being RO
+        mount -t btrfs "UUID=${UUID}" "${ram_dir}"
+        mount -o remount,rw "${ram_dir}"
+        umount "${ram_dir}"
+
         mount -t tmpfs cowspace ${ram_dir}
         mkdir -p ${ram_dir}/{upper,work}
         mount -t overlay -o lowerdir=${root_mnt},upperdir=${ram_dir}/upper,workdir=${ram_dir}/work rootfs ${root_mnt}
