d12582e2d48c12e93d52d9a5a1e77d2bada075f6
manuel <manuel@endeavouros.com>
2025-11-13 13:46:00 +0200

    [eos-update] code cleanup

--- eos-update/eos-update-other.bash
+++ eos-update/eos-update-other.bash
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+_ColorLines() {
+    # Print a message with the given "color" (see also /bin/eos-color).
+    # A message may include multiple lines in separate positional parameters.
+    # Message lines will be indented.
+
+    local color="$1"   # trailing positional paramemeters are the lines of the message
+    shift
+    eos-color "$color" 2
+    printf "==> %s\n" "$1" >&2
+    shift
+    [ "$1" ] && printf "    %s\n" "$@" >&2
+    eos-color reset 2
+}
+
 # You may comment out the following tip.
-GREEN=$'\e[0;92m'
-RESET=$'\e[0m'
-echo "${GREEN}==> Tip: you may add other update commands into ${0}.${RESET}" >&2
+_ColorLines info "Tip: you may add other update commands into ${0}."
 
 # Add your other update commands (e.g. for 'flatpak') here!
