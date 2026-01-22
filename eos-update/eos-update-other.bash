139475e6100fd2150c9c39a06f5aedfd3a2a5ce8
manuel <manuel@endeavouros.com>
2026-01-22 14:45:29 +0200

    [eos-update] expanding features of option --other-updates

--- eos-update/eos-update-other.bash
+++ eos-update/eos-update-other.bash
@@ -1,20 +1,40 @@
 #!/bin/bash
 
-_ColorLines() {
-    # Print a message with the given "color" (see also /bin/eos-color).
-    # A message may include multiple lines in separate positional parameters.
-    # Message lines will be indented.
+Main() {
+    ## Run your commands at various points in time during eos-update.
+    case "$1" in
+        beginning)
+            ## HERE: Add your commands to be executed in the beginning of eos-update.
+            ## Example idea: ranking mirrors.
+            ;;
+        end)
+            ## HERE: Add your commands to be executed in the end of eos-update.
+            ## Example idea: updates with flatpak or other tools.
+            ;;
+    esac
 
-    local color="$1"   # trailing positional paramemeters are the lines of the message
-    shift
+    ## You may comment out this tip:
+    _ColorLines info "Tip: you may add other update commands into ${0}."
+
+    ## HERE: Add your other update commands (e.g. for 'flatpak') here!
+    ## Note: This is deprecated, use the 'end)' branch of the 'case..esac' above instead.
+}
+
+_ColorLines() {
+    ## Print a message with the given "color" (see also /bin/eos-color).
+    ## A message may include multiple lines via positional parameters.
+    ## The lines after the first will be indented, i.e. the trailing positional paramemeters ($2, $3, ...)
+    ## will become the additional lines of the message.
+
+    local -r color="$1"
+    local -r first_line="$2"
+    local -r head="==>"
+    local -r head2="${head//?/ }"
+    shift 2
     eos-color "$color" 2
-    printf "==> %s\n" "$1" >&2
-    shift
-    [ "$1" ] && printf "    %s\n" "$@" >&2
+    printf "${head}  %s\n" "$first_line" >&2
+    [ "$1" ] && printf "${head2} %s\n" "$@" >&2      # other lines of the message
     eos-color reset 2
 }
 
-# You may comment out the following tip.
-_ColorLines info "Tip: you may add other update commands into ${0}."
-
-# Add your other update commands (e.g. for 'flatpak') here!
+Main "$@"
